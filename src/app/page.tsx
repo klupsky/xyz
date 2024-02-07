b9faa18783e36549780e3d90c84fd5c6a18e9e02
klupsky <katharina@chalupsky.eu>
2024-02-07 11:18:51 +0100

    setup components folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,11 @@ import './styles/reset.css'
 import data from './lib/data.js'
 import { GlobalStyles } from './styles/GlobalStyles'
 
-console.log(data, 'data')
-
 export default function Home() {
   return (
     <>
       <GlobalStyles />
-      <main>hello</main>
+      <main></main>
     </>
   )
 }
