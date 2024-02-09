0f3cf090cdbc40acb30078f3ffb0ccf81484a00a
klupsky <katharina@chalupsky.eu>
2024-02-09 11:25:37 +0100

    circle with dot in the center following mouse

--- src/app/components/Cursor/component.tsx
+++ src/app/components/Cursor/component.tsx
@@ -1,14 +1,54 @@
-'use client'
-import { motion, useMotionValue, useSpring } from 'framer-motion'
-import { useEffect, useState } from 'react'
-import { Circle } from './styles'
+import { motion } from 'framer-motion'
+import React, { useEffect, useState } from 'react'
+
+const CleanPercentage = (percentage) => {
+  const isNegativeOrNaN = !Number.isFinite(+percentage) || percentage < 0
+  const isTooHigh = percentage > 100
+  return isNegativeOrNaN ? 0 : isTooHigh ? 100 : +percentage
+}
+
+const Circle = ({ percentage, colour }) => {
+  const r = 21
+  const dotRadius = 2
+  const circ = 2 * Math.PI * r
+  const strokePct = ((100 - percentage) * circ) / 100
+
+  return (
+    <g>
+      <circle
+        r={r}
+        cx={21}
+        cy={21}
+        fill="transparent"
+        stroke={strokePct !== circ ? colour : ''}
+        strokeWidth="1px"
+        strokeDasharray={circ}
+        strokeDashoffset={percentage ? strokePct : 0}
+        opacity="0.9"
+      ></circle>
+      <circle r={dotRadius} cx={21} cy={21} fill="white"></circle>
+    </g>
+  )
+}
+
+const Pie = ({ percentage }) => {
+  const pct = CleanPercentage(percentage)
+
+  return (
+    <svg width={43} height={43}>
+      <g transform={`rotate(-90 ${'21 21'})`}>
+        <Circle colour="black" percentage={pct} />
+        <Circle colour="var(--c-white)" percentage={pct} />
+      </g>
+    </svg>
+  )
+}
 
 const Cursor = () => {
   const [mousePosition, setMousePosition] = useState({
     x: 0,
     y: 0,
   })
-  const [cursorVariant, setCursorVariant] = useState('default')
 
   useEffect(() => {
     const mouseMove = (e) => {
@@ -27,18 +67,18 @@ const Cursor = () => {
 
   const variants = {
     default: {
-      x: mousePosition.x - 16,
-      y: mousePosition.y - 16,
+      x: mousePosition.x - 21,
+      y: mousePosition.y - 21,
     },
   }
 
   return (
     <motion.div
       variants={variants}
-      animate={cursorVariant}
+      animate="default"
       style={{ zIndex: 5, position: 'absolute' }}
     >
-      <Circle />
+      <Pie percentage={50} />
     </motion.div>
   )
 }
--- src/app/components/Cursor/styles.ts
+++ src/app/components/Cursor/styles.ts
@@ -1,10 +1,13 @@
 import styled from 'styled-components'
 
-export const Circle = styled.div`
-  pointer-events: none;
-  height: 32px;
-  width: 32px;
+export const CircleCenter = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   border-radius: 50%;
   background-color: var(--c-white);
   z-index: 5;
+  height: 2px;
+  width: 2px;
 `
