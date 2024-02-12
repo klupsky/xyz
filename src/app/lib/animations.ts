22608187a660030f4406b9e9301b9c33b90d093b
klupsky <katharina@chalupsky.eu>
2024-02-12 14:36:23 +0100

    changed animation offset

--- src/app/lib/animations.ts
+++ src/app/lib/animations.ts
@@ -27,7 +27,7 @@ export const CenterImageVariants = {
 
 export const TitleVariants = {
   enter: {
-    y: 100,
+    y: 500,
     opacity: 0,
     transition: {
       duration: 0.5,
@@ -43,7 +43,7 @@ export const TitleVariants = {
     },
   },
   exit: {
-    y: -100,
+    y: -500,
     opacity: 0,
     transition: {
       duration: 0.5,
